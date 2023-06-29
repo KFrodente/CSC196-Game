19ca450c990e48b376e5124370a08dbbf0c3ed0d
KFrodente <kianfrodente@gmail.com>
2023-06-29 14:33:15 -0600

    renderer, timer, memory

--- Source/Engine/Core/Memory.cpp
+++ Source/Engine/Core/Memory.cpp
@@ -3,14 +3,39 @@
 
 using namespace std;
 
-void* operator new (size_t size) {
-	cout << "Allocated: " << size << endl;
+yogi::MemoryTracker yogi::g_memoryTracker;
 
-	return malloc(size);
+void* operator new (size_t size) {
+	void* p = malloc(size);
+
+	yogi::g_memoryTracker.Add(p, size);
+
+	return p;
 }
 
 void operator delete (void* memAddress, size_t size) {
-	cout << "Deallocated: " << size << endl;
-
+	yogi::g_memoryTracker.Remove(memAddress, size);
 	free(memAddress);
+}
+
+namespace yogi {
+
+	void MemoryTracker::Add(void* address, size_t size)
+	{
+		m_bytesAllocated += size;
+		m_numAllocations++;
+	}
+	
+	void MemoryTracker::Remove(void* address, size_t size)
+	{
+		m_bytesAllocated -= size;
+		m_numAllocations--;
+	}
+	
+	void MemoryTracker::DisplayInfo()
+	{
+		cout << "current bytes allocated: " << m_bytesAllocated << endl;
+		cout << "current number of allocations: " << m_numAllocations << endl;
+	}
+
 }
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <chrono>
 #include "Core/Memory.h"
 #include "Core/Random.h"
 #include "Core/FileIO.h"
+#include "Core/Time.h"
+#include "Renderer/Renderer.h"
 
 using namespace std;
 
@@ -9,28 +12,42 @@ using namespace std;
 
 int main() 
 {
+	yogi::g_memoryTracker.DisplayInfo();
 
 	int* p = new int;
+	yogi::g_memoryTracker.DisplayInfo();
 	
 	delete(p);
+	yogi::g_memoryTracker.DisplayInfo();
+	
+	yogi::MemoryAllocation timer;
+	for (int i = 0; i < 10000000; i++) {}
 
 
-	cout << yogi::getFilePath() << endl;
-	yogi::setFilePath("assets");
-	cout << yogi::getFilePath() << endl;
-	size_t size;
 
-	yogi::getFileSize("game.txt", size);
-	cout << size << endl;
+	auto start = std::chrono::high_resolution_clock::now();
 
-	std::string s;
-	yogi::readFile("game.txt", s);
+	for (int i = 0; i < 10000000; i++) {}
+	auto end = std::chrono::high_resolution_clock::now();
 
-	cout << s << endl;
+	cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << endl;
 
-	yogi::seedRandom((unsigned int)time(nullptr));
-	/*for (int i = 0; i < 3; i++)
-	{
-		cout << yogi::random(10, 12) << endl;
-	}*/
+	//cout << yogi::getFilePath() << endl;
+	//yogi::setFilePath("assets");
+	//cout << yogi::getFilePath() << endl;
+	//size_t size;
+
+	//yogi::getFileSize("game.txt", size);
+	//cout << size << endl;
+
+	//std::string s;
+	//yogi::readFile("game.txt", s);
+
+	//cout << s << endl;
+
+	//yogi::seedRandom((unsigned int)time(nullptr));
+	///*for (int i = 0; i < 3; i++)
+	//{
+	//	cout << yogi::random(10, 12) << endl;
+	//}*/
 }
